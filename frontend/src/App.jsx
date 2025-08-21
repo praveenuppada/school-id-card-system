a9f409c5a62603ef7c1eb1c18f512dc8be9c4511
uppada praveen <praveen.uppada1812@gmail.com>
2025-08-21 14:33:05 +0530

    All files replaced6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import { AuthProvider } from "./contexts/AuthContext.jsx"
 import WelcomePage from "./pages/WelcomePage.jsx"
 import LoginPage from "./pages/LoginPage.jsx"
